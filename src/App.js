006c30ad6f9ce37b5a67d7d078dba50797847d63
LauraBraun <77805783+LauraBraun@users.noreply.github.com>
2021-06-25 12:42:46 +0200

    Added icon and footer

--- src/App.js
+++ src/App.js
@@ -6,9 +6,14 @@ function App() {
     <div className="App">
       <div className= "container">
       <header className="App-header">
-       My Dictionary App
+      <i className="fas fa-book-reader"></i>
+      <h1 className="mb-5">Dictionary</h1>
       </header>
       <Search />
+      <br />
+      <footer>
+        Coded by Laura Braun. Open source on <a href="https://github.com/LauraBraun/dictionary-app" target="_blank" rel="noreferrer">Git-Hub</a>.
+      </footer>
       </div>
     </div>
   );
