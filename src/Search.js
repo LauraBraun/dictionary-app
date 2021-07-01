7c03d224e4a41833d730c4a6170b5a36a39b2b02
LauraBraun <77805783+LauraBraun@users.noreply.github.com>
2021-07-01 18:12:24 +0200

    added photos with pexel api

--- src/Search.js
+++ src/Search.js
@@ -1,20 +1,31 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Result from "./Result"; 
+import Photos from "./Photos";
 import "./Search.css"
 
 export default function Search () {
     const [word, setWord] = useState("");
     const [result, setResult] = useState(null);
+    const [photos, setPhotos] = useState(null);
 
     function handleResponse (response) {
         setResult(response.data[0]);
+    }
+
+    function handlePexelResponse(response) {
+        setPhotos(response.data.photos);
     }
 
     function handleSearch (event) {
         event.preventDefault();
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${word}`;
         axios.get(apiUrl).then(handleResponse);
+
+        let pexelsApiKey = "563492ad6f9170000100000145426b4d6890422e9ab296f9b6ee0d9a";
+        let pexelsUrl =`https://api.pexels.com/v1/search?query=${word}&per_page=6`;
+        let headers = {Authorization: `Bearer ${pexelsApiKey}`};
+        axios.get(pexelsUrl, { headers: headers }).then(handlePexelResponse);
     }
 
     function handleWord (event) {
@@ -28,5 +39,6 @@ export default function Search () {
         </form>
         </section>
         <Result info={result}/>
+        <Photos photos={photos} />
     </div>)
 }
